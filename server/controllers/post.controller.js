7988d5085bc49ae10c7fb50030ef25ac24635ac0
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2023-03-20 23:43:42 +0900

    erc20 fix error

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,3 +1,7 @@
+const Web3 = require("web3");
+const erc20abi = require("../contracts/erc20abi");
+const { User } = require("../models");
+
 exports.detail_get = async (req, res, next) => {
   try {
     res.status(200).send("hello world");
@@ -8,7 +12,31 @@ exports.detail_get = async (req, res, next) => {
 
 exports.register_post = async (req, res, next) => {
   try {
-    res.status(200).send("hello world");
+    // 1. session에서 로그인한 유저 정보, req.body에서 title, content 받아오기
+    const { id } = JSON.parse(req.session.user);
+    const { title, content } = req.body;
+    // 2.
+
+    // const nickname = req.session.user.nickname;
+
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+    const incrementToken = await user.increment("eth_amount", { by: 10 });
+    // 4. 블록체인 상에서 토큰 전송
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider("http://127.0.0.1:8555")
+    );
+
+    const contract = new web3.eth.Contract(erc20abi, process.env.ERC20_CA);
+    const result = await contract.methods
+      .transfer(user.address, 10)
+      .send({ from: process.env.SERVER_ADDRESS });
+    console.log("성공했습니다");
+
+    res.status(200).send(JSON.stringify(user));
   } catch (e) {
     throw Error(e);
   }
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,8 @@
-const { User } = require("../models");
+const { User, Post, Nft } = require("../models");
 const Web3 = require("web3");
 const bcrypt = require("bcrypt");
+const erc20abi = require("../contracts/erc20abi");
+const erc721abi = require("../contracts/erc721abi");
 const saltRounds = 10;
 
 exports.join_post = async (req, res, next) => {
@@ -25,7 +27,14 @@ exports.join_post = async (req, res, next) => {
       new Web3.providers.HttpProvider("http://127.0.0.1:8555")
     ); // 본인 가나슈 주소
     const address = await web3.eth.personal.newAccount(password);
+    // server에게 erc20 토큰 사용권한 주기
+    const contract = new web3.eth.Contract(erc20abi, process.env.ERC20_CA);
+    const approve = await contract.methods.approve(
+      process.env.SERVER_ADDRESS,
+      10000
+    );
     console.log(address);
+
     // 4. 비밀번호를 해싱
     const newpassword = await bcrypt.hash(password, saltRounds);
     console.log(newpassword);
@@ -95,7 +104,49 @@ exports.login_post = async (req, res, next) => {
 
 exports.transfer_post = async (req, res, next) => {
   try {
-    res.status(200).send("hello world");
+    // 1. front에서 데이터 받아오기
+    const { to, amount } = req.body;
+    // 2. cookie에서 받은 session id로 db 에서 로그인한 유저의 정보 받아오기
+    // (session에는 로그인 하는 과정의 db 정보만 저장하고 있기 때문에, 로그인 후 글을 써서 토큰이 늘어나 있을 가능성 있으므로
+    // db에서 찾는게 정확하다.)
+    const userInfoBySession = JSON.parse(req.session.user);
+    const user = await User.findOne({
+      where: { id: userInfoBySession.id },
+    });
+
+    // 3-1. 보내는 유저가 db 상에서 충분한 토큰이 있다면 계속 진행
+    if (user.eth_amount >= amount) {
+      // 4. 블록체인 상에서 토큰 전송
+      const web3 = new Web3(
+        new Web3.providers.HttpProvider("http://127.0.0.1:8555")
+      );
+
+      const contract = new web3.eth.Contract(erc20abi, process.env.ERC20_CA);
+      console.log(user.address);
+      //
+
+      web3.eth.getAccounts((err, accounts) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(accounts); // 이더리움 노드에 등록된 모든 계정 목록을 출력합니다
+        }
+      });
+
+      const unlockAccount = await web3.eth.personal.unlockAccount(
+        user.address,
+        "1234",
+        600
+      );
+      console.log("unlock :", unlockAccount);
+      const result = await contract.methods
+        .transferFrom(user.address, to, amount)
+        .send({ from: process.env.SERVER_ADDRESS });
+      console.log("성공했습니다");
+    } else {
+      // 3-2. db 상에 토큰 부족하면 프론트에 "잔액이 부족합니다" 전송
+      return res.status(400).send("토큰이 부족합니다!");
+    }
   } catch (e) {
     throw Error(e);
   }
@@ -103,7 +154,9 @@ exports.transfer_post = async (req, res, next) => {
 
 exports.faucet_get = async (req, res, next) => {
   try {
-    res.status(200).send("hello world");
+    // 1. session에서 user 주소 받아오기
+    const { address } = JSON.parse(req.session.user);
+    console.log(address);
   } catch (e) {
     throw Error(e);
   }
@@ -111,7 +164,30 @@ exports.faucet_get = async (req, res, next) => {
 
 exports.mypage_get = async (req, res, next) => {
   try {
-    res.status(200).send("hello world");
+    // 1. 세션으로부터 user id 정보 받아오기
+    const { id } = JSON.parse(req.session.user);
+    // 2. DB에서 user 정보, post,nft 정보(user가 만든) 다 받아오기
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+    const posts = await Post.findAll({
+      where: {
+        user_id: id,
+      },
+    });
+    const nfts = await Nft.findAll({
+      where: {
+        user_id: id,
+      },
+    });
+
+    // 3. 프론트에 보내주기
+    console.log("user", user);
+    console.log("posts", posts);
+    console.log("nfts", nfts);
+    return res.status(200).send(user, posts, nfts);
   } catch (e) {
     throw Error(e);
   }
