9ddb823385c96ae7ea1706418644603dff5fae9c
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2023-03-21 17:42:59 +0900

    create user,main Controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,38 +1,34 @@
-const { User, Post, Nft } = require("../models");
-const Web3 = require("web3");
-const bcrypt = require("bcrypt");
-const erc20abi = require("../contracts/erc20abi");
-const erc721abi = require("../contracts/erc721abi");
+const {User, Post, Nft} = require('../models');
+const Web3 = require('web3');
+const bcrypt = require('bcrypt');
+const erc20abi = require('../contracts/erc20abi');
+const erc721abi = require('../contracts/erc721abi');
 const saltRounds = 10;
+const BN = require('bn.js');
 
 exports.join_post = async (req, res, next) => {
   try {
     // 1. front에서 데이터 받아오기
-    const { nickname, password } = req.body;
+    const {nickname, password} = req.body;
     // 2. db에 같은 nickname 있는지 확인.없으면 계속 진행
     const exists = await User.findOne({
-      attributes: ["nickname"],
+      attributes: ['nickname'],
       where: {
         nickname,
       },
     });
-    console.log("여기요", exists);
     // 2-1.  있으면 종료
     if (exists) {
-      return res.status(400).send("동일한 nickname이 이미 존재합니다.");
+      return res.status(400).send('동일한 nickname이 이미 존재합니다.');
     }
     // 2-2. 없으면 계속 진행
     // 3. web3 사용해 가나슈 네트워크에 접속 후, 사용자의 비번을 이용한 지갑 생성
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8555")
-    ); // 본인 가나슈 주소
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8555')); // 본인 가나슈 주소
+    //
     const address = await web3.eth.personal.newAccount(password);
     // server에게 erc20 토큰 사용권한 주기
     const contract = new web3.eth.Contract(erc20abi, process.env.ERC20_CA);
-    const approve = await contract.methods.approve(
-      process.env.SERVER_ADDRESS,
-      10000
-    );
+    const approve = await contract.methods.approve(process.env.SERVER_ADDRESS, 10000);
     console.log(address);
 
     // 4. 비밀번호를 해싱
@@ -45,12 +41,13 @@ exports.join_post = async (req, res, next) => {
       password: newpassword,
       address,
       eth_amount: 0,
+      token_amount: 0,
     });
     // 6. 프론트에 성공적으로 회원가입이 완료되었음을 알림
     res
       .status(200)
       .send(
-        `회원가입을 성공했습니다, nickname :${nickname}, password :${newpassword} address :${address}`
+        `회원가입을 성공했습니다, nickname :${nickname}, password :${newpassword} address :${address}`,
       );
 
     // const user = await User.findAll({
@@ -67,8 +64,8 @@ exports.join_post = async (req, res, next) => {
 exports.login_post = async (req, res, next) => {
   try {
     // 1. front에서 데이터 받아오기
-    const { nickname, password } = req.body;
-    // 2-1. db에서 nickname이 일치하는 user가 있는지 확인
+    const {nickname, password} = req.body;
+    // 2-1. db에서 nickname, password이 일치하는 user가 있는지 확인
     const nicknameMatch = await User.findOne({
       where: {
         nickname,
@@ -76,26 +73,23 @@ exports.login_post = async (req, res, next) => {
     });
     // 2-2. nickname이 일치하는 계정이 있다면, 사용자가 입력한 비번과 해싱한 비번이 매치하는지 확인
     if (nicknameMatch) {
-      const match = await bcrypt.compare(
-        password,
-        nicknameMatch.dataValues.password
-      );
+      const match = await bcrypt.compare(password, nicknameMatch.dataValues.password);
       // 2-3. 비밀번호도 일치한다면, 로그인 성공을 프론트에 보내줌
       if (match) {
+        // 이 순간, 세션 활성화 하면서 브라우저(프론트)에 자동으로 쿠키를 심어줌
+        // 쿠키 내에 session id가 들어있고, 이 session id를 db의 session 테이블에 집어넣음
         req.session.loggedIn = true;
         req.session.user = JSON.stringify(nicknameMatch.dataValues);
         console.log(req.session);
-        return res
-          .status(200)
-          .send(`로그인 성공 ! : ${JSON.stringify(nicknameMatch.dataValues)}`);
+        return res.status(200).send(`로그인 성공 ! : ${JSON.stringify(nicknameMatch.dataValues)}`);
       } else {
         // 비밀번호만 일치하지 않는다면, "비밀번호를 확인하세요" 프론트에 보내줌
-        return res.status(400).send("비밀번호를 확인하세요");
+        return res.status(400).send('비밀번호를 확인하세요');
       }
 
       // 3. nickname이 일치하는 계정이 없다면 "닉네임을 확인하세요" 프론트에 보내줌
     } else {
-      return res.status(400).send("닉네임을 확인하세요");
+      return res.status(400).send('닉네임을 확인하세요');
     }
   } catch (e) {
     throw Error(e);
@@ -105,21 +99,19 @@ exports.login_post = async (req, res, next) => {
 exports.transfer_post = async (req, res, next) => {
   try {
     // 1. front에서 데이터 받아오기
-    const { to, amount } = req.body;
+    const {to, amount} = req.body;
     // 2. cookie에서 받은 session id로 db 에서 로그인한 유저의 정보 받아오기
     // (session에는 로그인 하는 과정의 db 정보만 저장하고 있기 때문에, 로그인 후 글을 써서 토큰이 늘어나 있을 가능성 있으므로
     // db에서 찾는게 정확하다.)
     const userInfoBySession = JSON.parse(req.session.user);
     const user = await User.findOne({
-      where: { id: userInfoBySession.id },
+      where: {id: userInfoBySession.id},
     });
 
     // 3-1. 보내는 유저가 db 상에서 충분한 토큰이 있다면 계속 진행
     if (user.eth_amount >= amount) {
       // 4. 블록체인 상에서 토큰 전송
-      const web3 = new Web3(
-        new Web3.providers.HttpProvider("http://127.0.0.1:8555")
-      );
+      const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8555'));
 
       const contract = new web3.eth.Contract(erc20abi, process.env.ERC20_CA);
       console.log(user.address);
@@ -133,19 +125,15 @@ exports.transfer_post = async (req, res, next) => {
         }
       });
 
-      const unlockAccount = await web3.eth.personal.unlockAccount(
-        user.address,
-        "1234",
-        600
-      );
-      console.log("unlock :", unlockAccount);
+      const unlockAccount = await web3.eth.personal.unlockAccount(user.address, '1234', 600);
+      console.log('unlock :', unlockAccount);
       const result = await contract.methods
         .transferFrom(user.address, to, amount)
-        .send({ from: process.env.SERVER_ADDRESS });
-      console.log("성공했습니다");
+        .send({from: process.env.SERVER_ADDRESS});
+      console.log('성공했습니다');
     } else {
       // 3-2. db 상에 토큰 부족하면 프론트에 "잔액이 부족합니다" 전송
-      return res.status(400).send("토큰이 부족합니다!");
+      return res.status(400).send('토큰이 부족합니다!');
     }
   } catch (e) {
     throw Error(e);
@@ -154,9 +142,36 @@ exports.transfer_post = async (req, res, next) => {
 
 exports.faucet_get = async (req, res, next) => {
   try {
-    // 1. session에서 user 주소 받아오기
-    const { address } = JSON.parse(req.session.user);
+    // 1. session에서 user address, id 받아오기
+    const {address, id} = JSON.parse(req.session.user);
     console.log(address);
+    // 2. server계정에서 user 주소로 ETH 1 보내주기
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8555'));
+    const myNumber = '1';
+    const myUnit = 'ether';
+    const myValue = new BN(await web3.utils.toWei(myNumber, myUnit));
+
+    // const faucetAmount = await web3.utils.toWei(1)
+    const result = await web3.eth.sendTransaction({
+      from: process.env.SERVER_ADDRESS,
+      to: address,
+      value: myValue,
+    });
+    // 3. db에서 id로 user를 찾고, eth_amount 올려주기
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+
+    const incrementEth = await user.increment('eth_amount', {by: 1});
+    // 4. 프론트로 보내주기
+    const resultUser = await User.findOne({
+      where: {
+        id,
+      },
+    });
+    return res.status(200).send(`0.1ETH 지급 완료, 잔액 :${resultUser.eth_amount}`);
   } catch (e) {
     throw Error(e);
   }
@@ -165,7 +180,7 @@ exports.faucet_get = async (req, res, next) => {
 exports.mypage_get = async (req, res, next) => {
   try {
     // 1. 세션으로부터 user id 정보 받아오기
-    const { id } = JSON.parse(req.session.user);
+    const {id} = JSON.parse(req.session.user);
     // 2. DB에서 user 정보, post,nft 정보(user가 만든) 다 받아오기
     const user = await User.findOne({
       where: {
@@ -184,9 +199,9 @@ exports.mypage_get = async (req, res, next) => {
     });
 
     // 3. 프론트에 보내주기
-    console.log("user", user);
-    console.log("posts", posts);
-    console.log("nfts", nfts);
+    console.log('user', user);
+    console.log('posts', posts);
+    console.log('nfts', nfts);
     return res.status(200).send(user, posts, nfts);
   } catch (e) {
     throw Error(e);
