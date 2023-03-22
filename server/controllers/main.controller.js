79d9645f99c8f84198ffa530c65b412d17d0a8c3
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2023-03-22 11:40:21 +0900

    complete database, main/user/register_post controllers

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -1,20 +1,13 @@
 const {Post} = require('../models');
 
+// 홈페이지
 exports.main_get = async (req, res, next) => {
   try {
     // 1. db에서 모든 post 가져오기
     const posts = await Post.findAll({});
 
     // 2. 프론트에 보내주기
-    res.status(200).send({message: 'HELLO WORLD!', data: posts});
-  } catch (e) {
-    throw Error(e);
-  }
-};
-
-exports.test_get = async (req, res, next) => {
-  try {
-    res.status(200).send('hello world');
+    res.status(200).send({message: '홈페이지', data: posts});
   } catch (e) {
     throw Error(e);
   }
