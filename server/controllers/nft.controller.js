1da156705b94f09b44723c220e039ffde34be206
bloud411 <bloud411@gmail.com>
2023-03-23 11:22:13 -0700

    23-03-23

--- server/controllers/nft.controller.js
+++ server/controllers/nft.controller.js
@@ -1,14 +1,10 @@
 const Web3 = require('web3');
-const { User } = require('../models');
+const { User,Nft } = require('../models');
 const web3 = new Web3('HTTP://127.0.0.1:7545');
 const erc20abi = require('./erc20Abi');
 const erc721abi = require('./erc721Abi');
-const erc20Contract = new web3.eth.Contract(erc20abi,'0x719Da13eC5F3ABfbF827DeF26De519BF0ad6efAe');
-const erc721Contract = new web3.eth.Contract(erc721abi,'0xE961F7B10f6ab0A78beaE6eF1cC272625BfD8a84');
-const main_addr = '0xa966996188a2259f4Fd0E413aa0082eEd2Ef3566';
-const sub1_addr = '0x74F25D84178175e58a10E15B921658898041643b'
-const sub2_addr = '0x922ffe88C3248b49dCe5d81Ff018Fd767c141c28'
-const tokenURL = 'https://ipfs.io/ipfs/QmdZB1QnQyLMiq7fFvzV8ier9znr8n9XfQSyH6oVjc9UQn'
+const erc20Contract = new web3.eth.Contract(erc20abi,process.env.ERC20_CA);
+const erc721Contract = new web3.eth.Contract(erc721abi,process.env.ERC721_CA);
 
 
 
@@ -19,27 +15,30 @@ exports.minting_post = async (req, res, next) => {
     //세션 정보
     const userInfoBySession = JSON.parse(req.session.user);
     //setToken하고 mintNFT
-    const setToken_In = await erc721Contract.methods.setToken('0x719Da13eC5F3ABfbF827DeF26De519BF0ad6efAe').send({from:main_addr,gas:100000});
+    const setToken_In = await erc721Contract.methods.setToken(process.env.ERC20_CA)
+    .send({from:process.env.SERVER_ADDRESS,gas:100000});
+    //setToken이 무사히 작동했으면 NFT를 발급합니다.
     if(setToken_In){
-      const tokenNumber = await erc721Contract.methods.mintNFT(sub1_addr,tokenurl,'hi2','예외적으로')
-      .send({from:main_addr,gas: '5000000'})
+      const tokenNumber = await erc721Contract.methods.mintNFT(userInfoBySession.address,tokenurl,'hi2','예외적으로')
+      .send({from:process.env.SERVER_ADDRESS,gas: '5000000'})
       .on('receipt', function(receipt){console.log('receipt')});
-      console.log("제대로 발행 되었습니다. : ",tokenNumber);
+      //NFT가 무사히 발행되었다면 서버 계정에 NFT발급 수수료 만큼 erc20토큰을 전송합니다.
       if(tokenNumber){
-        await erc20Contract.methods.transfer(main_addr,100).send({from:sub1_addr}).on('receipt', function(recipt){
-          console.log('ERC20토큰 전송!!!!!!')
-        });
+        await erc20Contract.methods.transfer(process.env.SERVER_ADDRESS,100)
+        .send({from:userInfoBySession.address})
+        .on('receipt', function(recipt){});
+        //발급한 NFT전체 수를 가져옵니다.
         const token_id = await erc721Contract.methods.getTotalSupply().call();
-
-        const result = Nft.create({
+        //NFT 데이터베이스에 각종 정보들을 넣어 줍니다.
+        const result =await Nft.create({
           user_id:userInfoBySession.id,
           token_id:token_id,
           name,
           description,
           tokenurl,
-          
+          tx_hash:"asdabfccdt",
         });
-
+        //로그인한 계정의 지갑에 erc20 토큰의 수를 가져와 데이터베이스에 업데이트 해줍니다.
         const token_amount = await erc20Contract.methods.balanceOf(userInfoBySession.address).call();
 
         User.update({
@@ -49,12 +48,6 @@ exports.minting_post = async (req, res, next) => {
         })
       }
     }
-
-    // const user = await User.findOne({
-    //   where: {id: userInfoBySession.id},
-    // });
-
-  
     res.status(200).send("hello world");
   } catch (e) {
     throw Error(e);
@@ -63,8 +56,8 @@ exports.minting_post = async (req, res, next) => {
 
 exports.market_get = async (req, res, next) => {
   try {
-    // const accounts = await web3.eth.getAccounts();
-    // const recipient = accounts[1];
+    const accounts = await web3.eth.getAccounts();
+    const recipient = accounts[1];
     const main_P = await erc20Contract.methods.balanceOf(main_addr).call();
     const total = await erc20Contract.methods.balanceOf(sub1_addr).call();
     const total2 = await erc20Contract.methods.balanceOf(sub2_addr).call();
